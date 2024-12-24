184834683244d908a55ed036591b6b0b28a3d1d3
Anas Othman <anasothman581@gmail.com>
2024-12-24 10:59:56 +0300

    string end

--- 1_Python_Basics/Section_1._Fundamentals/3_string.py
+++ 1_Python_Basics/Section_1._Fundamentals/3_string.py
@@ -1,3 +1,5 @@
+
+from sys import _xoptions
 from rich.console import Console
 
 console = Console()
@@ -70,17 +72,164 @@ console.print(str_len)
 
 
 str4 = "Python String"
-console.print(str4[0:4])
+console.print(str4[0:20: 2])
+
+#? string[start:end:jump]
+
+
+str5 = "C" + str4[1:] if str4.lower().startswith("p") else str4
+console.print(str5)
+
+
+s = "Hello World"
+
+print(s.upper())   # output: HELLO WORLD
+print(s.lower())   # output: hello world
+
+
+s = "  gfa   a"
+console.print(s.strip(" a"))
+console.print(s.lstrip(" a"))
+console.print(s.rstrip(" a"))
+
+
+s = "Python is fun are so fundemental"
+# Replaces 'fun' with 'awesome' it will replase even if 'fun' is part of another word
+print(s.replace("fun", "awesome"))
+
+import re
+
+s = "Python is fun are so fundemental"
+# Replace only the standalone word "fun"
+result = re.sub(r"\bfun\b", "awesome", s)
+print(result)
+
+
+s = "Hello "
+print(s * 3)
+
+name = "Alice"
+age = 22
+print(f"Name: {name}, Age: {age}")
 
 
 
+print("-------------------------------------------")
+#? python string method
+
+# Python3 program to show the
+# working of upper() function
+text = 'geeKs For geEkS'
+
+# upper() function to convert
+# string to upper case
+print("\nConverted String: upper")
+print(text.upper())
+
+# lower() function to convert
+# string to lower case
+print("\nConverted String: lower")
+print(text.lower())
+
+# converts the first character to 
+# upper case and rest to lower case 
+print("\nConverted String: title")
+print(text.title())
+
+# swaps the case of all characters in the string
+# upper case character to lowercase and viceversa
+print("\nConverted String: swapcase")
+print(text.swapcase())
+
+# convert the first character of a string to uppercase
+print("\nConverted String: capitalize")
+print(text.capitalize())
+
+# original string never changes
+print("\nOriginal String")
+print(text)
 
 
 
+text2 = "my name anas"
+print(text2.center(24,"-"))
 
 
 
+# count(substring, start index, end index) 
+c= "this text I use it for count method it count a number of letter in string like the string have 10:a"
+console.print(c.lower().count('a')) # => 3
+console.print(c.lower().count('b',1, 52)) # => 1
+console.print(c.lower().count('i')) # => 8
+console.print(c.count('i')) # => 7
 
 
 
+print("\nend with it return true and false\n")
+#? end with it return true and false
+text = "geeks for geeks."
+
+result = text.endswith('for geeks')
+print (result) # returns False
+result = text.endswith('geeks.')
+print (result) # returns True
+result = text.endswith('for geeks.')
+print (result) # returns True
+result = text.endswith('geeks for geeks.')
+print (result) # returns True
+
+
+
+#? finde and it case Sensitive we should use lower() 
+s = "Python programming"
+index = s.find("prog", 4)
+index = s.rfind("prog", 4)
+print(index)
+
+
+
+s = "Python programming"
+position = s.index("prog", 4)
+position = s.rindex("prog", 4)
+print(position)
+
+#? index and find dpo same thing but find return -1 if not found and index raise error if not found
+
+
+
+#? is all alphanumric
+s = "Python123"
+res = s.isalnum()
+console.print(res)
+
+
+
+#? isdecimal() Returns true if all characters in a string are decimal
+#? isdigit() Returns “True” if all characters in the string are digits
+#? isnumeric() Returns “True” if all characters in the string are numeric characters, and there is
+print("Ⅷ".isnumeric())
+
+print("\u0031".encode())
+
+
+
+jon = "anas othman"
+console.print("-".join(jon))
+
+
+
+#? partition
+string = "light attracts bug iy"
+print(string.partition(' '))
+print(string.rpartition(' '))
+
+string = "gold is heavy"
+# 'is' as partition
+print(string.partition('is'))
+
+
+url = "https://www.example.com/index.html"
+result = url.partition("//")
+result = result[2].partition("/")
+print(result[0]) #? output => www.example.com
 
