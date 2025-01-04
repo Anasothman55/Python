88b4f40a9b1108455297b6bd38bbb8a084c706a0
Anas Othman <anasothman581@gmail.com>
2025-01-04 20:47:51 +0300

    add Polymorphism

--- 6_OOP/2_Polymorphism.py
+++ 6_OOP/2_Polymorphism.py
@@ -26,6 +26,44 @@ print(add([1, 2], [3, 4])) # List concatenation
 
 
 
+class Dog:
+  def sound(self):
+    return "Bark"
+class Cat:
+  def sound(self):
+    return "Meow"
+# Polymorphism in action
+animals = [Dog(), Cat()]
+for animal in animals:
+  print(animal.sound())
+
+
+
+
+
+
+class Shape:
+  def area(self):
+    raise NotImplementedError("the shape can't have area")
+class Rectangle(Shape):
+  def __init__(self, length, width):
+    self.length = length
+    self.width = width
+  def area(self):
+    return self.length * self.width
+class Circle(Shape):
+  def __init__(self, radius):
+    self.radius = radius
+  def area(self):
+    return 3.14 * self.radius ** 2
+shapes = [Rectangle(2, 3), Circle(5)]
+for shape in shapes:
+  print(f"Area: {shape.area()}")
+
+
+
+
+
 
 
 
